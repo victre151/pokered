675784bf266b26311cb623589f6f4e7be9ebbda5
victre151 <victre.kanale@gmail.com>
2025-07-24 14:29:53 -0400

    title
    title edits

--- engine/movie/title.asm
+++ engine/movie/title.asm
@@ -396,7 +396,7 @@ INCLUDE "data/pokemon/title_mons.asm"
 
 ; prints version text (red, blue)
 PrintGameVersionOnTitleScreen:
-	hlcoord 4, 5, 6, 7, 8
+	hlcoord 7, 8
 	ld de, VersionOnTitleScreenText
 	jp PlaceString
 
